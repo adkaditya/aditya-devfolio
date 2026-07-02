20189a0adc0d4a046a613fd648e8f4062d55ae94
unknown <adk6535@gmail.com>
2026-07-02 23:51:15 +0530

    Debug API URL

--- backend/server.js
+++ backend/server.js
@@ -26,6 +26,7 @@ app.use(cors({
     "http://localhost:5174",
     "http://localhost:5175",
     "http://localhost:5176",
+     "https://YOUR-NETLIFY-SITE.netlify.app",
   ],
   credentials: true,
 }));
