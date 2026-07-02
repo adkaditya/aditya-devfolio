a0f613b6b9cfb58ca100669bd6f055d224a8d221
unknown <adk6535@gmail.com>
2026-07-02 23:55:51 +0530

    Fix CORS for Netlify

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,8 @@ app.use(cors({
     "http://localhost:5174",
     "http://localhost:5175",
     "http://localhost:5176",
-     "https://YOUR-NETLIFY-SITE.netlify.app",
+"https://aditya-devportfolio-pages.netlify.app",
+
   ],
   credentials: true,
 }));
